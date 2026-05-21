42e81e694510954d79ba23b8d84a87cb4a081c35
Jaishpatel09 <jaishpatel.ktr@gmail.com>
2026-05-21 15:09:24 +0530

    node

---
+++ JavaScript Clg/Day 12/index.js
@@ -0,0 +1,90 @@
+// import {a,sum} from './new.js'
+
+ 
+
+// console.log(a);
+// sum(20,25)
+
+
+
+// Pure Function
+// function sum(num){
+//     return num*2
+// }
+// console.log(sum(5));
+// console.log(sum(5));
+
+
+
+// Impure Function
+// let count=0
+// function outer(){
+//     count++
+//     return count
+// }
+// console.log(outer());
+
+
+// function sum(a){
+//     return function(b){
+//         return a+b
+//     }
+// }
+
+
+// console.log(sum(5)(9));
+
+
+// function sum(a){
+//     return function(b) {
+//         if(b){
+//             return sum(a+b)
+//         }
+//         return a
+//     }
+// }
+
+// console.log(sum(5)(6)(7)(8)(1)(2)(4)(6)());
+
+// console.log("Hello");
+// Spread Operator
+// let arr=[1,2,3,4,5]
+// let num=[...arr,6,7,8]
+
+// console.log(num);
+
+// By Object
+// let obj={
+//     id:1,
+//     name:"Jaish"
+// }
+// let object1={
+//     ...obj,
+//     age:101
+// }
+// console.log(object1);
+
+
+// function sum(a,b,c,...nums){ //...nums rest parameter
+//     console.log();
+    
+// }
+//  sum(1,2,3,4,5,6,7,8,9,45)
+//  if(true){
+//     let a=5
+//  }
+// console.log(a);
+
+
+
+// console.log("a");
+
+// new Promise(()=>{
+//     console.log("hii");
+    
+// })
+// console.log("Byyy");
+
+
+let arr=[]
+
---
+++ JavaScript Clg/Day 12/new.js
@@ -0,0 +1,11 @@
+let a=10
+
+function sum(c,d){
+    // console.log("Hello Dostoo");
+    console.log(c+d);
+    
+    
+}
+export {a,sum}
+
+// export default a
