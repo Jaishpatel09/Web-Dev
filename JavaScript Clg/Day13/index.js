739f88e188d95b03f890e7acb49d4b5807e442e0
Jaishpatel09 <jaishpatel.ktr@gmail.com>
2026-05-22 11:28:33 +0530

    Object Properties

---
+++ JavaScript Clg/Day13/index.js
@@ -0,0 +1,57 @@
+// let obj={
+//     id:2,
+//     firstname:"Jaish ",
+//     lastname:"Patel",
+//     fullname:function(city,age){
+//         console.log(this.firstname+this.lastname+city+age);
+
+// const { use } = require("react");
+
+        
+//     }
+// }
+// // obj.fullname()
+
+// let userOne={
+
+//     id:12,
+//     firstname:"Cockroach ",
+//     lastname:"Janata Party"
+    
+// }
+// // obj.fullname.call(userOne," Jabalpur")
+// obj.fullname.apply(userOne,[" Jabalpur ",100])
+
+
+// let obj={
+//     id:12,
+//     name:"Jaish"
+// }
+// let user=obj
+
+
+
+// let user1={
+//     name:"Yash",
+//     age:30,
+//     address:{
+//         city:"jabalpur"
+//     }
+// }
+
+// let user2=structuredClone(user1)
+// user2.address.city="Bhopal"
+
+// console.log(user1.address.city);
+// console.log(user2.address.city);
+
+
+// Hidden Properties Show karegaa
+let arr=[1,2,3,4]
+console.log(Object.prototype);
+
+
+
+
+
+
