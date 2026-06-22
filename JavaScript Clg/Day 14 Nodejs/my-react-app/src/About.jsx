e24da81a695fd154c5753880d8597d9796b3292e
Jaishpatel09 <jaishpatel.ktr@gmail.com>
2026-06-22 16:01:16 +0530

    RouterDom

--- JavaScript Clg/Day 14 Nodejs/my-react-app/src/About.jsx
+++ JavaScript Clg/Day 14 Nodejs/my-react-app/src/About.jsx
@@ -1,11 +1,9 @@
-// import React from 'react'
+import React from 'react'
 
-// const About = () => {
-//   return (
-//     <div>
-//       Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloremque voluptatum ipsam earum. Eligendi, nostrum ad commodi rem libero cumque et est aliquid illo enim eaque iure perferendis ipsum quidem consectetur sint ullam sit eos deserunt dicta tempore voluptatem fugiat sunt dolores. Expedita numquam dicta suscipit?
-//     </div>
-//   )
-// }
+const About = () => {
+  return (
+    <div>About👋●'◡'● </div>
+  )
+}
 
-// export default About
+export default About
--- JavaScript Clg/Day 14 Nodejs/my-react-app/src/App.jsx
+++ JavaScript Clg/Day 14 Nodejs/my-react-app/src/App.jsx
@@ -445,18 +445,18 @@
 //Basicss
 
 
-import React from 'react'
-import Basics from './Basics'
+// import React from 'react'
+// import Basics from './Basics'
 
-const App = () => {
-  return (
-    <div>
-      <Basics/>
-    </div>
-  )
-}
+// const App = () => {
+//   return (
+//     <div>
+//       <Basics/>
+//     </div>
+//   )
+// }
 
-export default App
+// export default App
 
 
 
@@ -829,3 +829,29 @@ export default App
 //   }
   
 //   export default App
+
+
+// React Router Dom
+
+
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './Home'
+import About from './About'
+import Contact from './Contact'
+const App = () => {
+  return (
+    <>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+      </Routes>
+
+      </>
+
+    
+  )
+}
+
+export default App
